427e4a5b64ce544cd2faee07af1461aa9eeade50
Parag Srivastava <57228563+paragsrivastava@users.noreply.github.com>
2023-09-12 00:14:07 -0400

    Update 2waygeocoder.py
    Indent fixed

--- 2waygeocoderfunction/2waygeocoder.py
+++ 2waygeocoderfunction/2waygeocoder.py
@@ -128,7 +128,7 @@ def lambda_handler(event, context):
         #########################################################
         elif "Address" in columns:
             for index, row in data.iterrows():
-                 try:
+                try:
                     json_response = ""
                     if 'Country' in columns and pd.isna(row.Country) == False:
                         response = location.search_place_index_for_text(
@@ -141,65 +141,65 @@ def lambda_handler(event, context):
                             Text= str(row.Address) + str(row.City) + "," + str(row.State) + "," + str(row.Zip))
                     json_response = response["Results"]
                     print(json_response)
-                 except:
+                except:
                     print("API Response Error")
-                 try:
+                try:
                     CountryCode = (json_response[0]["Place"]["Country"])
                     Countries.append(CountryCode)
-                 except:
+                except:
                     Countries.append("")
-                 try:
+                try:
                     Point = (json_response[0]["Place"]["Geometry"]["Point"])
                     Points.append(Point)
-                 except:
+                except:
                     Points.append("")
-                 try:
+                try:
                     Longitude.append(Point[0])
                     Latitude.append(Point[1])
-                 except:
+                except:
                     Longitude.append("")
                     Latitude.append("")
                     print("Error: Lat/Lon unavailable for given input in row", (len(Points)) + 1)
-                 try:
+                try:
                     Label = (json_response[0]["Place"]["Label"])
                     Labels.append(Label)
-                 except:
+                except:
                     Labels.append("")
                     print("Error: Address unavailable for given input in row", (len(Points)) + 1)
-                 try:
+                try:
                     AddressNumber = (json_response[0]["Place"]["AddressNumber"])
                     AddressNumbers.append(AddressNumber)
-                 except:
+                except:
                     AddressNumbers.append("")
-                 try:
+                try:
                     Street = (json_response[0]["Place"]["Street"])
                     Streets.append(Street)
-                 except:
+                except:
                     Streets.append("")
-                 try:
+                try:
                     if "Municipality" in (json_response[0]["Place"]):
                          Municipality = (json_response[0]["Place"]["Municipality"])
                          Municipalities.append(Municipality)
                     else:
                          Municipalities.append("")
-                 except:
+                except:
                      Municipalities.append("")
-                 try:
+                try:
                     Region = (json_response[0]["Place"]["Region"])
                     Regions.append(Region)
-                 except:
+                except:
                     Regions.append("")
                     print("Error: Region unavailable for given input in row", (len(Points)) + 1)
-                 try:
+                try:
                     SubRegion = (json_response[0]["Place"]["SubRegion"])
                     SubRegions.append(SubRegion)
-                 except:
+                except:
                     SubRegions.append("")
                     print("Error: SubRegion unavailable for given input in row", (len(Points)) + 1)
-                 try:
+                try:
                     Relevance = (json_response[0]["Relevance"])
                     Relevances.append(Relevance)
-                 except:
+                except:
                     Relevances.append("")
                     print("Error: Relevance unavailable for given input in row", (len(Points)) + 1)
     
